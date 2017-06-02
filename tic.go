2f37c4adb4a2582f0117384e634d6d4ae675b73c
Lars <lars.scheme@gmail.com>
2017-06-03 01:27:39 +0200

    Add option to customize printing

--- tic.go
+++ tic.go
@@ -21,6 +21,12 @@ Start and end messages are optional so the program:
 	}
 
 will only print the time and nothing else.
+
+You can change the Println function to whatever you want the output to be, if
+you set it to nil there will be no output. By default it is set to fmt.Println.
+Println is captured when calling Toc, this means it will use the value that
+Println had when calling Toc, even if you set Println to something else before
+the deferred timer function runs.
 */
 package tic
 
@@ -33,13 +39,21 @@ import (
 // call to the returned function. See the package documentation for further
 // information.
 func Toc(startMsg ...interface{}) func(endMsg ...interface{}) {
+	pl := Println
+	if pl == nil {
+		pl = dummyPrintln
+	}
 	if len(startMsg) > 0 {
-		fmt.Println(startMsg...)
+		pl(startMsg...)
 	}
 	start := time.Now()
 	return func(endMsg ...interface{}) {
 		dt := time.Now().Sub(start)
 		endMsg = append(endMsg, fmt.Sprint(dt))
-		fmt.Println(endMsg...)
+		pl(endMsg...)
 	}
 }
+
+var Println = fmt.Println
+
+func dummyPrintln(a ...interface{}) (int, error) { return 0, nil }
