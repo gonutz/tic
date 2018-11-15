cc2e5355832ada8438d7f589ae0fa6050cb677eb
Lars <lars.scheme@gmail.com>
2018-11-15 11:09:40 +0100

    Improve error handling and messages

--- cmd/toc/main.go
+++ cmd/toc/main.go
@@ -1,38 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
-	"fmt"
 	"github.com/gonutz/tic/cmd"
 )
 
 func main() {
+	corrupt := false
+	defer func() {
+		if corrupt {
+			fmt.Println("time stack file is corrupt, deleting it, please call tic again")
+			os.Remove(cmd.Path())
+		}
+	}()
 	now := time.Now()
 
 	f, err := os.OpenFile(cmd.Path(), os.O_RDWR, 0x666)
-	check(err)
+	if err != nil {
+		fmt.Println("call tic before toc")
+		return
+	}
 	defer f.Close()
 
 	_, err = f.Seek(-1, os.SEEK_END)
-	check(err)
+	if err != nil {
+		fmt.Println("call tic before toc")
+		return
+	}
 	var size [1]byte
 	_, err = f.Read(size[:])
-	check(err)
+	if err != nil {
+		corrupt = true
+		return
+	}
 	newEnd, err := f.Seek(-1-int64(size[0]), os.SEEK_END)
-	check(err)
+	if err != nil {
+		corrupt = true
+		return
+	}
 	data := make([]byte, size[0])
 	_, err = f.Read(data)
-	check(err)
+	if err != nil {
+		corrupt = true
+		return
+	}
 	var t time.Time
-	check(t.UnmarshalBinary(data))
+	err = t.UnmarshalBinary(data)
+	if err != nil {
+		corrupt = true
+		return
+	}
 	fmt.Println(now.Sub(t))
 	f.Truncate(newEnd)
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
